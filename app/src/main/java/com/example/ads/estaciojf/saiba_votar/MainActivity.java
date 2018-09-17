a2f943a462202b69bc059d110e4b254273680b54
Leandro Simões <lesimoes0@gmail.com>
2018-09-17 19:48:52 -0300

    1- Add firebase reference

--- app/src/main/java/com/example/ads/estaciojf/saiba_votar/MainActivity.java
+++ app/src/main/java/com/example/ads/estaciojf/saiba_votar/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.ads.estaciojf.saiba_votar;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        ValueEventListener msg = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        }
+
     }
 }
